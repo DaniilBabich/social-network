ca73c95efaf1d5a83cd2410070549e0ee7ba51d0
Daniil Babich <daniil.babich.1990@yandex.ru>
2020-11-03 03:40:26 +0500

    Updates

---
+++ precache-manifest.7f0692f1d8abe5c32211c554cfe57780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fe5830cdc096e6aa14977a1c1b2a9f",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "f7e17716fe7dd93d8a63",
+    "url": "/social-network/static/css/main.797ec97e.chunk.css"
+  },
+  {
+    "revision": "4a90dc8de0a3671432ff",
+    "url": "/social-network/static/js/2.7a45d9ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.7a45d9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e17716fe7dd93d8a63",
+    "url": "/social-network/static/js/main.a791c527.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  },
+  {
+    "revision": "baa67b120cf77b8dea7c6e70271d3446",
+    "url": "/social-network/static/media/defaultUserPhoto.baa67b12.png"
+  }
+]);
